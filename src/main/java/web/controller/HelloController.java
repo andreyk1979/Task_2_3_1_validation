caad121979d08d6e24d0ddc0a930feb67ee7fa89
andreyk1979 <andrey.kuimov@gmail.com>
2022-01-02 13:46:08 +0200

    Have done

--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -1,24 +1,80 @@
 package web.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+//import jdk.internal.module.IllegalAccessLogger;
+//import org.graalvm.compiler.lir.LIRInstruction;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+import web.dao.UserDAO;
+import web.models.User;
+
+
+import java.time.Period;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@RequestMapping()
 public class HelloController {
 
-	@GetMapping(value = "/")
-	public String printWelcome(ModelMap model) {
-		List<String> messages = new ArrayList<>();
-		messages.add("Hello!");
-		messages.add("I'm Spring MVC application");
-		messages.add("5.2.0 version by sep'19 ");
-		model.addAttribute("messages", messages);
-		return "index";
-	}
-	
+    private final UserDAO userDAO;
+
+    @Autowired
+    public HelloController(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
+    @GetMapping(value = "/")
+    public String sayWelcome(ModelMap model) {
+        List<String> messages = new ArrayList<>();
+        messages.add("Hello!");
+        messages.add("I'm Spring MVC application");
+        messages.add("5.2.0 version by sep'19 ");
+        model.addAttribute("messages", messages);
+        return "index";
+    }
+
+    @GetMapping("/user")
+    public String index(Model model) {
+        model.addAttribute("people", userDAO.index());
+        return "user";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        model.addAttribute("person", userDAO.show(id));
+        return "show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") User user) {
+        return "new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") User user) {
+        userDAO.save(user);
+        return "redirect:/user";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id) {
+        model.addAttribute("person", userDAO.show(id));
+        return "/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("person") User user, @PathVariable("id") int id) {
+        userDAO.update(id,user);
+        return "redirect:/user";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+      userDAO.delete(id);
+      return "redirect:/user";
+    }
 }
---
+++ src/main/java/web/dao/UserDAO.java
@@ -0,0 +1,44 @@
+package web.dao;
+
+import org.springframework.stereotype.Component;
+import web.models.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class UserDAO {
+    private static long PEOPLE_COUNT;
+    private List<User> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new User(++PEOPLE_COUNT, "Andrey1", "Kuimov1", "kuimov1@mail.ru"));
+        people.add(new User(++PEOPLE_COUNT, "Andrey2", "Kuimov2", "kuimov2@mail.ru"));
+        people.add(new User(++PEOPLE_COUNT, "Andrey3", "Kuimov3", "kuimov3@mail.ru"));
+        people.add(new User(++PEOPLE_COUNT, "Andrey4", "Kuimov4", "kuimov4@mail.ru"));
+    }
+
+    public List<User> index() {
+        return people;
+    }
+
+    public User show(int id) {
+        return people.stream().filter(user -> user.getId() == id).findAny().orElse(null);
+    }
+
+    public void save(User user) {
+        user.setId(++PEOPLE_COUNT);
+        people.add(user);
+    }
+
+    public void update(int id, User updateUser) {
+        User personToBeUpdated = show(id);
+        personToBeUpdated.setFirstName(updateUser.getFirstName());
+    }
+
+    public void delete(int id) {
+        people.removeIf(p -> p.getId() == id);
+    }
+
+}
